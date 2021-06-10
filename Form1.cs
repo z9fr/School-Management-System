3911dd9f36e4e66d7723ab8d3e0e9baf59e0d418
Dasith Vidanage <dasithvidanage000@gmail.com>
2021-06-09 20:26:35 -0700

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -17,6 +17,7 @@ namespace CollageManagementSystem
             InitializeComponent();
         }
         
+        
         private void label2_Click(object sender, EventArgs e)
         {
         }
